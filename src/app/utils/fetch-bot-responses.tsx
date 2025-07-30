224b22f6abb76c2b70bdf306ef43d4a33367090a
Kaique Prado <kiqprado@outlook.com>
2025-07-30 11:46:07 -0300

    fix.

--- src/app/utils/fetch-bot-responses.tsx
+++ src/app/utils/fetch-bot-responses.tsx
@@ -5,7 +5,7 @@ interface IMessage {
 
 export async function FetchMessageOnChat(message: string, language: 'pt' | 'en' | 'es' =  'pt'): Promise<IMessage | null> {
   try {
-    const response = await fetch('neatly-api-production.up.railway.app/chatweb', {
+    const response = await fetch('https://neatly-api-production.up.railway.app/chatweb', {
       method: 'POST',
       headers:  { 'Content-type': 'application/json'},
       body: JSON.stringify({ content: message, language})
